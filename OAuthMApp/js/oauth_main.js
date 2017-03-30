0e4a3135805e380253354fca3d0bd913abb25af0
Sergei Papulin <papulin_bmstu@mail.ru>
2017-03-30 21:53:41 +0300

    Initial

---
+++ OAuthMApp/js/oauth_main.js
@@ -0,0 +1,18 @@
+/*
+ * 
+ * https://developer.tizen.org/development/articles/facebook-app-tizen
+ * 
+ */
+
+(function(){
+	
+	var oauthMain = {};
+	
+	oauthMain.init = function() {
+
+		
+	};
+	
+	return oauthMain;
+	
+})().init();
